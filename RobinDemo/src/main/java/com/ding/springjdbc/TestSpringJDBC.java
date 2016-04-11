45a9927e3987f6b5f0690aded6a18b9e3749a720
robin <158070976@qq.com>
2016-04-11 22:13:19 +0800

    add first test file

---
+++ RobinDemo/src/main/java/com/ding/springjdbc/TestSpringJDBC.java
@@ -0,0 +1,11 @@
+package com.ding.springjdbc;
+
+public class TestSpringJDBC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+
+	}
+
+}
